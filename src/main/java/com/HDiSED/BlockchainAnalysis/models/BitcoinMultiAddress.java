dae9511230f50e75232bb36abab83dab76297bbd
AdrianAEI <adrian-oskar@wp.pl>
2023-06-25 13:23:51 +0200

    id fixed

--- src/main/java/com/HDiSED/BlockchainAnalysis/models/BitcoinMultiAddress.java
+++ src/main/java/com/HDiSED/BlockchainAnalysis/models/BitcoinMultiAddress.java
@@ -3,6 +3,8 @@ package com.HDiSED.BlockchainAnalysis.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
 import java.util.List;
@@ -11,6 +13,12 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Node("BitcoinMultiAddress")
 public class BitcoinMultiAddress {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private List<BitcoinSingleAddress> addresses;
+
     private List<BitcoinTransaction> txs;
 }
